adb3c2442cfca0e4d51d6fa5f95169651fabd14f
maritimefleet1 <maritimefleet1@gmail.com>
2026-03-01 08:56:26 -0800

    Production backend url updated

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -3,6 +3,6 @@ import { APP_ID } from "@angular/core";
 export const environment = {
   production: false,
   // Your Spring Boot Local URL
-  // apiUrl: 'https://vessel-tracking-backend.onrender.com' ,
-  apiUrl: 'http://localhost:8081/',
+  apiUrl: 'https://maritime-fleet-backend.onrender.com' ,
+  // apiUrl: 'http://localhost:8081/',
 };
